7242cab6a74c968ed65aa19b9ac142d9a9627574
DennisN22042003 <dennisnjuguna278@gmail.com>
2025-04-03 15:45:04 +0300

    Initial git commit on the local git repository.

---
+++ src/main/java/com/example/bloggingplatformAPI/BloggingplatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bloggingplatformAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingplatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingplatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bloggingplatformAPI/BloggingplatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bloggingplatformAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingplatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
