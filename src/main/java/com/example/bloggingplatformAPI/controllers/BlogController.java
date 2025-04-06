890a3856cbb71417e630d4f9988d55a01ab5275c
DennisN22042003 <dennisnjuguna278@gmail.com>
2025-04-07 01:44:59 +0300

    Created the controller class to handle the various API calls related to a Blog post.

--- src/main/java/com/example/bloggingplatformAPI/controllers/BlogController.java
+++ src/main/java/com/example/bloggingplatformAPI/controllers/BlogController.java
@@ -1,4 +1,14 @@
 package com.example.bloggingplatformAPI.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.bloggingplatformAPI.repositories.BlogRepository;
+
+@Controller
+@RequestMapping("/api/v1/")
 public class BlogController {
+    private BlogRepository blogRepository;
+
+    // TODO: Set up endpoints to POST, GET, PUT, DELETE a blog post...
 }
